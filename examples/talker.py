ce34e292d008b126892f0594e92c06547be4eb5f
Yixiang Gao <yixiangg310573@gmail.com>
2025-08-12 14:55:20 -0500

    add create_timer

--- examples/talker.py
+++ examples/talker.py
@@ -1,22 +1,20 @@
-import time
 from simpleros import Node
 from simpleros.msg.std_msg import String
+
+
+def timer_callback(publisher, i):
+    msg = String(f"hello world {i[0]}")
+    publisher.publish(msg)
+    print(f"TALKER SENDING: '{msg.data}'")
+    i[0] += 1
 
 
 def main():
     with Node("talker") as talker_node:
         publisher = talker_node.create_publisher("chatter", String)
-        i = 0
-
-        try:
-            while True:
-                msg = String(f"hello world {i}")
-                publisher.publish(msg)
-                print(f"TALKER SENDING: '{msg.data}'")
-                i += 1
-                time.sleep(1)
-        except KeyboardInterrupt:
-            pass
+        i = [0]  # in order to pass by reference instead of value
+        talker_node.create_timer(1.0, timer_callback, publisher, i)
+        talker_node.spin()
 
 
 if __name__ == "__main__":
